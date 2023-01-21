d693b62e718f0a1c6534d97faa33d435256ed734
Aaron Howser <aaronhowser1@gmail.com>
2023-01-21 05:27:56 -0600

    more

--- Topics/BigDecimal/Round and power/src/Main.kt
+++ Topics/BigDecimal/Round and power/src/Main.kt
@@ -3,5 +3,12 @@ import java.math.RoundingMode
 
 fun main() {
     // write your code here
+    val power = readln().toInt()
+    val mode = readln().toInt()
+    val number = readln().toBigDecimal()
 
+    var output = number.setScale(mode, BigDecimal.ROUND_FLOOR)
+    output = output.pow(power)
+
+    println(output)
 }
